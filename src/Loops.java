15aeb4cc0705505d4eabb6ae13a8ad8396790f37
yoga-user <m.rzezutka@gmail.com>
2020-03-29 22:06:05 +0200

    b

---
+++ src/Loops.java
@@ -0,0 +1,17 @@
+public class Loops {
+    public static void main(String[] arg) {
+        for (int i=0; i<5; i++) {
+            System.out.println("i= " + i);
+        }
+
+        for (int j=10; j>2; j--) {
+            System.out.println("j=" + j);
+        }
+        for (int n=1;n<=5;n++){
+            System.out.println("In row is " +n);
+        }
+
+
+    }
+
+}
